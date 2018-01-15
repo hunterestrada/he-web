496250d90d9aa86ca7366144aac5d30a6365a0a3
hunterestrada <hunterestrada@icloud.com>
2018-01-14 19:41:46 -0600

    Improves bad-route view

--- src/components/errors/BadRouteView.js
+++ src/components/errors/BadRouteView.js
@@ -10,10 +10,16 @@ const NAME_VIEW_ROUTE_BAD = style.getContentComponentClassName([
   'BadRouteView'
 ])
 
+const NAME_VIEW_CONTENT = style.getClassName([
+  style.NAME_CARD, 'ContentView'
+]);
+
 const BadRouteView = () => (
   <div className={NAME_VIEW_ROUTE_BAD}>
-    <h1>{string.title()}</h1>
-    <h2>{string.subtitle()}</h2>
+    <div className={NAME_VIEW_CONTENT}>
+      <h1>{string.title()}</h1>
+      <h2>{string.subtitle()}</h2>
+    </div>
   </div>
 )
 
